347188181ce7cda878f9ebe99e6d5509ba476eca
Danuta <dsstanczak@gmail.com>
2015-01-18 17:44:53 +0100

    function code added

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,52 @@
 #The inverse is calculating only in case 
 #it is not stored in the cache.
 
-## Function to create a dummy matrix object
+## Function creates a special matrix object
 #to cache its inverse
+#contain methods to get and set the inverse in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    #setters and getters
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    
+    #set solve in cache
+    setsolve <- function(solve) m <<- solve
+    
+    #get solve from cache
+    getsolve <- function() m
+    
+    #list of defined functions
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Function to get a matrix inverse
 #gets one argument - a inversible matrix x
 #returns one object - a matrix which is the inverse of x
+#inverse is taken from the cache if it already exists there
+#if not it is calculated and send to the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## check if inverse is cached
+    m <- x$getsolve()
+    
+    ##if yes then return it from cache
+    if(!is.null(m)) {
+        message("getting cached inverse")
+        return(m)
+    }
+    
+    ##otherwise it calculates the inverse and set in cache
+    message("calculating inverse...")
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
